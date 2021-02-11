a7e2459ac0dccbec59d11e593798f7661d541f00
RenattoAlencar <alencar.renato@yahoo.com.br>
2021-02-11 01:52:57 -0300

    feat(assets,pages, component header and routes)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
 
+import Routes from './routes'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello Word</h1>
-      </header>
-    </div>
+    <Routes/>
   );
 }
 
